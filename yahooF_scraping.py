937101ceaf014a7622889919439452ef18401d3b
ACLXRD <andres.lopez01@correo.usa.edu.co>
2021-10-15 13:26:53 -0500

    Se agregan comentarios y se automatiza para obtener el csv de las compañias propuestas

--- yahooF_scraping.py
+++ yahooF_scraping.py
@@ -1,23 +1,7 @@
+from datetime import datetime, timedelta
 import csv
 import urllib.request
 from io import StringIO
-# import datetime
-
-# # current date and time
-# today = datetime.date.today()
-# yesterday = today - datetime.timedelta(days=1)
-# yesterdayplus = today - datetime.timedelta(days=2)
-
-# # yesterday = yesterday.strftime("%H:%M:%S")
-# print("Hoy:", today)
-# print("Ayer:", yesterday)
-# print("Antier:", yesterdayplus)
-
-# timestamp1 = yesterday.timestamp()
-# timestamp2 = datetime.datetime.timestamp(yesterdayplus)
-# print("timestamp de ayer =", timestamp1)
-# print("timestamp de antier =", timestamp2)
-from datetime import datetime, timedelta
 
 # current date and past dates
 today = datetime.now().replace(hour = 0, minute = 0, second = 0, microsecond = 0)
